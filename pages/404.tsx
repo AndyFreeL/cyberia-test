ccce42ddb26fe8403801eeb9b488e142ee0362b7
AndyFreeL <andy00freelance@gmail.com>
2023-05-06 19:57:43 +0700

    fix

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,5 @@
+import styles from '../styles/Button.module.scss'
+
 const Error = () => {
     return (
 <>
@@ -5,7 +7,7 @@ const Error = () => {
                 <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', height:'100vh'}}>
                     <img style={{paddingBottom:58}} src="/405.svg" alt=""/>
                     <span style={{paddingBottom:58}}>Страница не найдена</span>
-                    <a href={'/projects'} className='send__button'>Перейти на главную</a>
+                    <a href={'/projects'} className={styles.send__button}>Перейти на главную</a>
                 </div>
 
 </>
